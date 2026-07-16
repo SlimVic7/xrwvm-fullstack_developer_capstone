2620171222cae96a055f5520baf7cc7ce9f4914b
SlimVic7 <your.email@example.com>
2026-07-16 12:54:10 -0400

    update react front end

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,11 @@
+import Dealers from './components/Dealers/Dealers';
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+        <Route path="/dealers" element={<Dealers/>} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
